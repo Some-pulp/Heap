a5ad73e2ec09e721ef0fe7983adae9e8c081d10b
Some-pulp <34895585+Some-pulp@users.noreply.github.com>
2019-05-03 09:08:33 -0700

    Heap Header File

---
+++ Heap.h
@@ -0,0 +1,38 @@
+//
+//  Heap.h
+//  Heap
+//
+//  Created by Peter ODonnell on 5/3/19.
+//  Copyright © 2019 Peter ODonnell. All rights reserved.
+//
+//  Heap.h implements a largest max-heap
+
+#include "node.h"
+
+
+using namespace std;
+
+#ifndef Heap_h
+#define Heap_h
+
+class Heap{
+private:
+    node* root;
+    int* arr;
+    int counter = 0;
+    int size;
+public:
+    Heap();
+    Heap(int size);
+    ~Heap();
+    
+    void insert(int v);
+    int remove();
+    int largest();
+    
+    void resize();
+    void bubbleUp(int i);
+    int trickleDown();
+};
+
+#endif /* Heap_h */
